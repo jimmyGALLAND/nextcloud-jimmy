0d2a94e711536c2335a0ff6005bac81c1ec2f822
Jimmy GALLAND <jimmy.galland@celjim.fr>
2022-12-12 21:07:08 +0100

    add version 25

--- build.sh
+++ build.sh
@@ -1,23 +1,38 @@
+if [ -z "${BASH_SOURCE}" ] ; then
+    echo "You need to execute this script using bash v4+ without using pipes"
+    exit 1
+fi
+
+cd "${BASH_SOURCE%/*}/" || exit 1
+
 NC_VERS=$(curl -s https://api.github.com/repos/nextcloud/server/releases/latest \
 | grep "tag_name" \
 | cut -d : -f 2 \
 | tr -d "\",v " \
 )
 
-
 if [ -z ${NC_VERS} ]; then
-	exit 1
+ exit 1
 fi
 
-docker build --build-arg NC_VERS=${NC_VERS} --no-cache  -t jimmygalland/nextcloud:latest -t jimmygalland/nextcloud:${NC_VERS} . && \
-docker push jimmygalland/nextcloud:${NC_VERS} && \
-docker push jimmygalland/nextcloud:latest
+if [ ! -f "./version" ]; then
+ echo "-" > "./version"
+fi
 
+VERS=$(<"./version")
+MAJOR_VERS=$( cut -d'.' -f 1  <<< "${NC_VERS}")
 
-#docker builder prune -af
+if [ ${NC_VERS} != ${VERS} ]; then
+ if [[ ${MAJOR_VERS} != 24 && ${MAJOR_VERS} != 25 ]]; then
+  exit
+ fi
 
-#docker login --username jimmygalland
-#docker tag [TAG] jimmy.galland/nextcloud:latest
-#docker push jimmygalland/nextcloud:latest
+ cd $MAJOR_VERS
+ docker build --build-arg NC_VERS=${NC_VERS} --no-cache  -t jimmygalland/nextcloud:latest -t jimmygalland/nextcloud:${NC_VERS} . && \
+ docker push jimmygalland/nextcloud:${NC_VERS} && \
+ docker push jimmygalland/nextcloud:latest && \
+ echo "${NC_VERS}" > "../version"
+ cd -
 
+fi
 
